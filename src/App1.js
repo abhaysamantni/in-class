16bfc8966542be5e90bc29140a52812f9da4afc4
abhaysamantut <abhay.samant@gmail.com>
2026-01-16 22:10:17 -0600

    update code

---
+++ src/App1.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App1(props) {
+   return (
+      <div> 
+      <h1>{props.name}</h1>
+      <p>{props.title}</p>
+      </div>
+   );
+   };
+
+export default App1;
